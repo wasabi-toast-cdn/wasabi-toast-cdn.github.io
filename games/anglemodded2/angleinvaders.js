95f12ac091317d7149ce71e4793834b59098e003
thehtml5gameguy <barstaxjolster@gmail.com>
2025-05-05 11:47:47 -0700

    Update angleinvaders.js

--- games/anglemodded2/angleinvaders.js
+++ games/anglemodded2/angleinvaders.js
@@ -373,13 +373,13 @@ function show_angle() {
 function check_answer() {
     if (shooting || !playing) return;
     entered_angle = parseInt($('#shoot_angle').text());
-    show_Popup("DEBUG: "+entered_angle, 1000)
+    show_popup("DEBUG: "+entered_angle, 1000)
     if (Number.isNan(entered_angle) == true) {
         clear_shoot_angle();
         shooting = false;
         return;
     }
-    show_Popup("DEBUG: "+entered_angle, 1000)
+    show_popup("DEBUG: "+entered_angle, 1000)
     $('#player_ship,#missile').css('transform', 'rotate(' + (entered_angle - 90) + 'deg)');
     play_sound(0);
     $('#missile').css({
